d180a14a3957ae941dab11d4b245639cc0d766b4
Jonathan Ung <jonathan.keith.ung@gmail.com>
2022-05-12 16:23:56 -0700

    added onigiri, movement

--- ninjaman.js
+++ ninjaman.js
@@ -2,7 +2,12 @@ var world = [];
 var worldDict = {
     0: 'blank',
     1: 'wall',
-    2: 'sushi'
+    2: 'sushi',
+    3: 'onigiri'
+}
+var ninjaman = {
+    x: 1,
+    y: 1
 }
 const size = 10;
 
@@ -14,7 +19,7 @@ function generateWorld() {
                 world[row][col] = 1;
                 // console.log(world[row][col]);
             } else {
-                world[row][col] = Math.floor(Math.random() * 3);
+                world[row][col] = Math.floor(Math.random() * 4);
                 // console.log(world[row][col]);
             }
         }
@@ -37,5 +42,34 @@ function drawWorld() {
     worldHTML.innerHTML = out;
 }
 
+function drawNinjaman() {
+    document.getElementById('ninjaman').style.top = ninjaman.y * 40 + 'px'
+    document.getElementById('ninjaman').style.left = ninjaman.x * 40 + 'px'
+}
+
+document.onkeydown = function(e) {
+    if (e.keyCode == 65) { // LEFT
+        if (world[ninjaman.y][ninjaman.x - 1] != 1) {
+            ninjaman.x--;
+        }
+    } else if (e.keyCode == 68) { // RIGHT
+        if (world[ninjaman.y][ninjaman.x + 1] != 1) {
+            ninjaman.x++;
+        }
+    } else if (e.keyCode == 83) { // DOWN
+        if (world[ninjaman.y + 1][ninjaman.x] != 1) {
+            ninjaman.y++;
+        }
+    } else if (e.keyCode == 87) { // UP
+        if (world[ninjaman.y - 1][ninjaman.x] != 1) {
+            ninjaman.y--;
+        }
+    }
+    world[ninjaman.y][ninjaman.x] = 0;
+    drawNinjaman();
+    drawWorld();
+}
+
 generateWorld();
 drawWorld();
+drawNinjaman();
